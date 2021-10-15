66e7ac4d7148b02dc4f5320e8a0b2cb435650e61
Nikolay Popov <nikolay.cn.popov@gmail.com>
2021-10-15 17:00:15 +0300

    Added Plus and Plus divided examples and full Plus divided tables of formal and effective accuracies.

--- src/main/kotlin/com/nhekfqn/dd/doubledicetest/Main.kt
+++ src/main/kotlin/com/nhekfqn/dd/doubledicetest/Main.kt
@@ -2,9 +2,9 @@ package com.nhekfqn.dd.doubledicetest
 
 import kotlin.random.Random
 
-fun main() {
-    val iterationsNumber = 1_000_000
+val iterationsNumber = 10_000_000
 
+fun main() {
     // SIMPLE
 
     var simplePositiveCasesCounter = 0
@@ -17,7 +17,7 @@ fun main() {
 
     val simplePositiveCaseProbability = simplePositiveCasesCounter.toDouble() / iterationsNumber
 
-    println("Simple '>= 20' Probability: $simplePositiveCaseProbability")
+    println("Simple '>= 20' Probability: $simplePositiveCaseProbability  ")
 
     // ONE-BY-ONE
 
@@ -39,7 +39,7 @@ fun main() {
 
     val oneByOnePositiveCaseProbability = oneByOnePositiveCasesCounter.toDouble() / iterationsNumber
 
-    println("One-by-one '>= 20' probability: $oneByOnePositiveCaseProbability")
+    println("One-by-one '>= 20' probability: $oneByOnePositiveCaseProbability  ")
 
     // DOUBLE DICE
 
@@ -56,5 +56,60 @@ fun main() {
 
     val doubleDicePositiveCaseProbability = doubleDicePositiveCasesCounter.toDouble() / iterationsNumber
 
-    println("Double dice '>= 10' probability: $doubleDicePositiveCaseProbability")
+    println("Double dice '>= 10' probability: $doubleDicePositiveCaseProbability  ")
+
+    // PLUS
+
+    var plusPositiveCasesCounter = 0
+
+    for (i in 1..iterationsNumber) {
+        if (Random.nextInt(50) + Random.nextInt(50) >= 20) {
+            plusPositiveCasesCounter++
+        }
+    }
+
+    val plusPositiveCaseProbability = plusPositiveCasesCounter.toDouble() / iterationsNumber
+
+    println("Plus '>= 20' probability: $plusPositiveCaseProbability  ")
+
+    // PLUS DIVIDED
+
+    var plusDividedPositiveCasesCounter = 0
+
+    for (i in 1..iterationsNumber) {
+        if ((Random.nextInt(100) + Random.nextInt(100)) / 2 >= 20) {
+            plusDividedPositiveCasesCounter++
+        }
+    }
+
+    val plusDividedPositiveCaseProbability = plusDividedPositiveCasesCounter.toDouble() / iterationsNumber
+
+    println("Plus divided '>= 20' probability: $plusDividedPositiveCaseProbability  ")
+
+    // EFFECTIVE ACCURACIES TABLES
+
+    println("\nEffective accuracies table (step 0.05):\n")
+
+    for (i in 0..100 step 5) {
+        val formalAccuracy = i.toDouble() / 100
+        println("${formalAccuracy.toString().padEnd(6)}: ${toEffectiveAccuracy(formalAccuracy)}  ")
+    }
+
+    println("\nEffective accuracies table (step 0.005):\n")
+
+    for (i in 0..200) {
+        val formalAccuracy = i.toDouble() / 200
+        println("${formalAccuracy.toString().padEnd(6)}: ${toEffectiveAccuracy(formalAccuracy)}  ")
+    }
+}
+
+fun toEffectiveAccuracy(formalAccuracy: Double): Double {
+    var count = 0
+    for (i in 1..iterationsNumber) {
+        if ((Random.nextDouble() + Random.nextDouble()) / 2 < formalAccuracy) {
+            count++
+        }
+    }
+
+    return count.toDouble() / iterationsNumber
 }
